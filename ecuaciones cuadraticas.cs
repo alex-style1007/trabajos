7294563ef0686b243a6d189bcb17df528848056d
alex-style1007 <68159727+alex-style1007@users.noreply.github.com>
2020-07-21 12:09:22 -0500

    Add files via upload

---
+++ ecuaciones cuadraticas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ecuacion_de_segundo_grado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tutoria 4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tutoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ingreso de datos
+            int a= int.Parse(Console.ReadLine());//votos partido 1
+            int B = int.Parse(Console.ReadLine());//votos partido 2
+            int blancos = int.Parse(Console.ReadLine());//votos en blanco
+            int anulados = int.Parse(Console.ReadLine());//votos anulados
+            int n = int.Parse(Console.ReadLine());//total de la poblacion
+            double p = int.Parse(Console.ReadLine());//porcentaje mayor de edad
+
+            int abs = (int)(n * p) - (a + B + blancos + anulados);
+            bool C1 = anulados <(a+B)*0.3;
+            bool C2 = (a * B) > blancos;
+            bool C3 =  abs< n;
+            if ((C1 || C2) && C3)
+            {
+                Console.WriteLine("LAS VOTACIONES FUERON EXITOSAS");
+                if (a > B) { Console.WriteLine("El partido 1 es el ganador"); 
+                }
+                else { Console.WriteLine("El partido 2 es el ganador"); }
+                
+            }
+            
+        }
+    }
+}
